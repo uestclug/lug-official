8fb0815ae5081d542bf08c7e0aa1bbf6d2e658ea
lolipop <lolipop0703@qq.com>
2020-11-02 02:16:47 +0800

    Add serves view.
    & flex improvement.

--- src/components/Home/Serves.vue
+++ src/components/Home/Serves.vue
@@ -1,11 +1,73 @@
 <template>
-  <div>
-    Serves
-  </div>
+  <v-row>
+    <v-col
+      cols="6"
+      md="4"
+      lg="3"
+      v-for="serve in serves"
+      v-bind:key="serve.name"
+      class="text-center"
+    >
+      <v-icon
+        class="clickable serve-icon pb-4"
+        color="primary"
+      >{{ serve.icon }}</v-icon>
+      <h3
+        class="clickable"
+      >{{ serve.name }}</h3>
+      <small>{{ serve.intro }}</small>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 export default {
   name: 'Serves',
+  data: () => ({
+    serves: [
+      {
+        name: '开源镜像站',
+        icon: 'fas fa-compact-disc',
+        intro: '成电开源镜像站',
+        path: '',
+      },
+      {
+        name: 'IPTV 直播',
+        icon: 'fas fa-headset',
+        intro: 'IPTV，好的 TV',
+        path: '',
+      },
+      {
+        name: '宿舍网络报修',
+        icon: 'fas fa-tools',
+        intro: '您的寝室生活救星',
+        path: '',
+      },
+      {
+        name: 'CAT ROOM',
+        icon: 'fas fa-cat',
+        intro: '摸鱼聊天留言板',
+        path: '',
+      },
+      {
+        name: 'ISSUES',
+        icon: 'fas fa-tasks',
+        intro: 'Bug 提交/意见反馈',
+        path: '',
+      },
+      {
+        name: '',
+        icon: '',
+        intro: '',
+        path: '',
+      },
+    ],
+  }),
 };
 </script>
+
+<style scoped>
+.serve-icon {
+  font-size: 84px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,21 +3,21 @@
     <ContentTitle class="content" title="线上服务" icon="fas fa-concierge-bell" />
     <Serves class="content" />
     <v-row class="content">
-      <v-col cols="12" md="6">
+      <v-col cols="12" lg="6">
         <ContentTitle title="新闻公告" icon="fas fa-bullhorn" />
         <NewsList />
       </v-col>
-      <v-col cols="12" md="6">
+      <v-col cols="12" lg="6">
         <ContentTitle title="博客文章" icon="fas fa-blog" />
         <BlogList />
       </v-col>
     </v-row>
     <v-row class="content">
-      <v-col cols="12" md="6">
+      <v-col cols="12" lg="6">
         <ContentTitle title="加入我们" icon="fas fa-user-plus" />
         <Recruitment />
       </v-col>
-      <v-col cols="12" md="6">
+      <v-col cols="12" lg="6">
         <ContentTitle title="协会成员" icon="fas fa-user-friends" />
         <Members />
       </v-col>
