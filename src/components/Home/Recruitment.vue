0c12b8fba23d28ae16ce1a06a19da7fcd739bbc8
YukariChiba <i@0x7f.cc>
2022-03-20 14:25:40 +0800

    Change NU descr to LUG descr

--- src/components/Home/Recruitment.vue
+++ src/components/Home/Recruitment.vue
@@ -11,13 +11,13 @@
             # WE NEED YOU
           </v-card-title>
           <v-card-text>
-            我们是成立{{ existYear }}年的学生组织，历经变迁，现隶属于信息中心，
-            参与校内网络基础设施维护工作，提供开源软件镜像等诸多服务。
+            我们是成立{{ existYear }}年的学生社团，积极参与高校开源社区活动，提供开源软件镜像等诸多服务。
           </v-card-text>
           <v-card-title> ## 我们要做的事 </v-card-title>
           <v-card-text>
-            1. 向校内外提供开源软件镜像、在线文档、IP 电视等服务；<br />
-            2. 维护两校区学生宿舍区的网络基础设施。
+            1. 推广 GNU/Linux 在校内外的使用。
+            2. 协助维护开源镜像站等关键服务。
+            3. 联合校内外爱好者
           </v-card-text>
           <v-card-title> ## 为何要加入我们 </v-card-title>
           <v-card-text>
@@ -27,16 +27,6 @@
             零距离接触生产环境网络设备的有趣经验；<br />
             勤工助学有偿服务机会。
           </v-card-text>
-          <v-card-title> ## 招新方向 </v-card-title>
-          <v-card-text>
-            <p>
-              我们面向<strong>电子科技大学的全体学生</strong>招新，包括以下四个方向：
-            </p>
-            <v-chip label color="primary" class="mr-2">网络</v-chip>
-            <v-chip label color="primary" class="mr-2">前端</v-chip>
-            <v-chip label color="primary" class="mr-2">后端</v-chip>
-            <v-chip label color="primary" class="mr-2">文宣</v-chip>
-          </v-card-text>
           <v-card-actions>
             <v-btn
               text
@@ -44,7 +34,7 @@
               :disabled="submitResumeLink == ''"
               @click="$Utils.openExternalLink(submitResumeLink)"
             >
-              投递简历
+              填写表单
             </v-btn>
             <v-btn
               text
@@ -76,7 +66,7 @@ export default {
   }),
   created() {
     // 计算成立时长
-    this.existYear = " " + (new Date().getFullYear() - 1997) + " ";
+    this.existYear = " " + (new Date().getFullYear() - 2019) + " ";
   },
 };
 </script>
